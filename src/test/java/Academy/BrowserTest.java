bfe95b85040f6b2013dd539ef0bb933c6c21944d
SanjayPG <32269442+SanjayPG@users.noreply.github.com>
2025-08-11 22:28:50 +0100

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -1,31 +1,23 @@
 package Academy;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
 public class BrowserTest {
 
-	
-	@Test
-	public void getData()
-	{
-		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
-		driver.get("https://learnautomation.azurewebsites.net/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
-		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("my devops Learing"));
-		driver.close();
-	
-		
-		
-	}
+    @Test
+    public void getData() {
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.get("https://learnautomation.azurewebsites.net/webapp/");
+            String text = driver.findElement(By.cssSelector("h1")).getText();
+            System.out.println(text);
+            Assert.assertTrue(text.equalsIgnoreCase("my devops Learing"));
+        } finally {
+            driver.quit();
+        }
+    }
 }
