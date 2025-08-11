805935be47ed031b39ae64fc5dbbb0a0eeb8b011
SanjayPG <32269442+SanjayPG@users.noreply.github.com>
2025-08-11 23:43:44 +0100

    Update BrowserTest.java

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -8,6 +8,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BrowserTest {
 
@@ -17,18 +21,28 @@ public class BrowserTest {
         WebDriver driver = new ChromeDriver();
         try {
             driver.get("https://www.google.com/");
-            WebElement searchBox = driver.findElement(By.name("q"));
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+            // Accept Google's consent dialog if present
+            try {
+                WebElement agreeButton = wait.until(ExpectedConditions.elementToBeClickable(
+                    By.xpath("//div[contains(@class, 'VfPpkd-RLmnJb') or @id='L2AGLb' or .='I agree' or .='Accept all']")));
+                agreeButton.click();
+            } catch (Exception e) {
+                // If the dialog does not appear, continue
+            }
+
+            // Now wait for the search box to be visible and interactable
+            WebElement searchBox = wait.until(ExpectedConditions.elementToBeClickable(By.name("q")));
             searchBox.sendKeys("Selenium");
-            searchBox.sendKeys(Keys.RETURN); // Press Enter
+            searchBox.sendKeys(Keys.RETURN);
 
-            // Wait for results page to load (simple sleep for demo; use WebDriverWait in real tests)
-            Thread.sleep(2000);
-
+            // Wait for the results page and check the title
+            wait.until(ExpectedConditions.titleContains("Selenium"));
             String title = driver.getTitle();
             System.out.println("Page title is: " + title);
             Assert.assertTrue("Title should contain 'Selenium'", title.contains("Selenium"));
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         } finally {
             driver.quit();
         }
